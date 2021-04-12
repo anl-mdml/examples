eb94b1ce98bac1ec143cc0af8388b1cb1e903f34
merfagxxavier0 <jelias1@luc.edu>
2021-04-12 11:51:09 -0500

    update rate testing script

--- scripts/kafka/producer.py
+++ scripts/kafka/producer.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='192.168.39.2:9092')
 
-for i in range(10):
+for i in range(1000):
     producer.send('test_topic', b'whoaoaoaoao')
     print(f'sent {i}')
