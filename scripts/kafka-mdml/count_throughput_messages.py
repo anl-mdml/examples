7a785df6cab3b0ae09314164656caf9ee20acc4a
Jakob Elias <jelias1@luc.edu>
2021-06-08 12:44:46 -0500

    changed host

--- scripts/kafka-mdml/count_throughput_messages.py
+++ scripts/kafka-mdml/count_throughput_messages.py
@@ -1,12 +1,18 @@
 import mdml_client as mdml
 
 consumer = mdml.kafka_mdml_consumer(
-    topics = ["mdml-examples-throughput"],
-    group = "mdml-examples-throughput")
+    topics = ["mdml-example-throughput"],
+    group = "mdml-examples-throughput",
+    kafka_host = "192.168.39.2",
+    kafka_port = 9093,
+    schema_host = "192.168.39.2"
+)
 
 i = 0
 
 for msg in consumer.consume():
     i += 1
+    if i%1000==0:
+        print(f"received {i} messages")
 
 print(f"Messages counted: {i}")
