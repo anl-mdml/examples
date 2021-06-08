beac1f2eb3003d53f12f4a5b1ff21f1d17a29b09
Jakob Elias <jelias1@luc.edu>
2021-06-08 09:46:06 -0500

    throughput test added

---
+++ scripts/kafka-mdml/test_message_throughput.py
@@ -0,0 +1,38 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--host", help="Kafka hostname",
+                        required=True)
+parser.add_argument("--port", help="Kafka port",
+                        required=True)
+parser.add_argument("--srhost", help="Schema registry hostname",
+                        required=True)
+parser.add_argument("--seconds", help="Number of seconds to send messages",
+                        required=True)
+parser.add_argument("--bytes", help="Number of bytes for the message",
+                        required=True)
+args = parser.parse_args()
+
+import time
+import mdml_client as mdml
+
+producer = mdml.kafka_mdml_producer(
+    topic = "mdml-example-throughput",
+    schema = "example_throughput.json",
+    kafka_host = args.host,
+    kafka_port = int(args.port),
+    schema_host = args.srhost
+)
+
+dat = {
+    'random_part': "A" * int(args.bytes)
+}
+i=0
+start = time.time()
+while time.time() - start < int(args.seconds):
+    dat['time'] = time.time()
+    producer.produce(dat)
+    i += 1
+end = time.time()
+
+print(f"Sent {i} messages with a random part size of {args.bytes} bytes in {end-start} seconds.")
+print(f"AVG Messages Per Second: {i/(end-start)}")
