09509358b5b89a0f6090c3e4a58f339604215f85
merfagxxavier0 <jelias1@luc.edu>
2021-06-08 08:55:32 -0500

    plif timing script updated for edge devices

--- scripts/kafka-mdml/example_plif_timing.py
+++ scripts/kafka-mdml/example_plif_timing.py
@@ -1,5 +1,7 @@
 import glob
 import time
+import botocore
+import boto3
 import mdml_client as mdml
 
 # Now that the FuncX function is running and ready to accept data, we will start sending data
@@ -7,16 +9,22 @@ s3_client = mdml.kafka_mdml_s3_client(
     topic = 'mdml-examples-plif', # S3 bucket is derived from topic as mdml-examples
     s3_endpoint = "https://s3.it.anl.gov:18082",
     s3_access_key = "JNLZP8P0XJE05KSO01LF",
-    s3_secret_key = "t3+1pwXh6DmiBdY4AKJuKutlIpR5FzxiqNcewu94"
+    s3_secret_key = "t3+1pwXh6DmiBdY4AKJuKutlIpR5FzxiqNcewu94",
+    kafka_host = "192.168.39.2",
+    kafka_port = 9093,
+    schema_host = "192.168.39.2"
 )
+
+print("client created")
 
 images = glob.glob("images/*.PLIF1.TIF")
 
 start = time.time()
 for img in images:
     s3_client.produce(
-        filepath = img, 
+        filepath = img,
         obj_name = img
     )
 end = time.time()
 print(f'Seconds to send 500 MB (50x10MB images) to S3 {end-start}')
+print(f'Transfer rate of roughly {500/(end-start)} MB/s')
