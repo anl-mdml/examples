d556e31f7a73ca8eeba46c1cc92e067b518be166
jelias1 <jelias1@luc.edu>
2021-06-24 16:02:45 -0500

    updated chunking examples

--- scripts/kafka-mdml/multipart_images_consumer.py
+++ scripts/kafka-mdml/multipart_images_consumer.py
@@ -5,27 +5,29 @@ import mdml_client as mdml
 consumer = mdml.kafka_mdml_consumer(
     topics = ["mdml-example-plif-multipart"],
     group = "mdml-example-plif-multipart")
+for filepath in consumer.consume_chunks(save_dir='streamed'):
+    print(filepath)
 
-files = {}
-for msg in consumer.consume():
-    value = msg['value']
-    fn = value['filename']
-    part_info = value['part'].split('.')
-    if fn in files:
-        files[fn][part_info[0]] = value['b64']
-    else:
-        files[fn] = {
-            'parts': part_info[1],
-            part_info[0]: value['b64'] 
-        }
-    if len(files[fn].keys()) == (int(files[fn]['parts']) + 1):
-        print("all parts received")
-        dat = ''
-        for i in range(int(files[fn]['parts'])):
-            dat += files[fn][str(i+1)]
-        dat_bytes = base64.b64decode(dat)
-        with open(f'streamed/{fn.split("/")[1]}', 'wb') as f:
-            f.write(dat_bytes)
-        # print(files[fn].keys())
-        print(time.time())
-        # del files[fn]
+# files = {}
+
+# for msg in consumer.consume():
+    # value = msg['value']
+    # fn = value['filename']
+    # part_info = value['part'].split('.')
+    # if fn in files:
+    #     files[fn][part_info[0]] = value['chunk']
+    # else:
+    #     files[fn] = {
+    #         'parts': part_info[1],
+    #         part_info[0]: value['chunk'] 
+    #     }
+    # if len(files[fn].keys()) == (int(files[fn]['parts']) + 1):
+    #     print("all parts received")
+    #     dat = ''
+    #     for i in range(int(files[fn]['parts'])):
+    #         dat += files[fn][str(i+1)]
+    #     dat_bytes = base64.b64decode(dat)
+    #     with open(f'streamed/{fn.split("/")[1]}', 'wb') as f:
+    #         f.write(dat_bytes)
+    #     print(time.time())
+    #     del files[fn]
--- scripts/kafka-mdml/multipart_images_producer.py
+++ scripts/kafka-mdml/multipart_images_producer.py
@@ -1,7 +1,5 @@
 import glob
-import math
 import time
-import base64
 import mdml_client as mdml
 
 images = glob.glob("images/*.PLIF1.TIF")
@@ -10,29 +8,13 @@ producer = mdml.kafka_mdml_producer(
     topic = "mdml-example-plif-multipart",
     schema = "example_plif_multipart.json")
 
-def send_image(fn, part_size, producer):
-    with open(fn, 'rb') as f:
-        dat_bytes = f.read()
-    dat_b64 = base64.b64encode(dat_bytes).decode('utf-8')
-    dat_len = len(dat_b64)
-    total_parts = math.ceil(dat_len/part_size)
-    part = 1
-    while len(dat_b64):
-        part_bytes = dat_b64[0:part_size]
-        dat_b64 = dat_b64[part_size:]
-        dat = {
-            'b64': part_bytes,
-            'part': f'{part}.{total_parts}',
-            'filename': fn
-        }
-        producer.produce(dat)
-        part += 1
-    producer.flush()
-
 start = time.time()
 print(f"streaming {len(images)} images")
 print(f"start: {start}")
 for img in images:
-    send_image(img, 100000, producer)
+    chunks = mdml.chunk_file(img, 100000)
+    for chunk in chunks:
+        producer.produce(chunk)
+producer.flush()
 end = time.time()
-
+print(f"end: {end}")
